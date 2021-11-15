16f0f40f235aa3c11e9d96ffd6c49870a4ee8604
Rodrigosolari <Rodrigo13_23@yahoo.es>
2021-11-15 01:32:55 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_courbet.mk
-
-COMMON_LUNCH_CHOICES := \
-    nad_courbet-user \
-    nad_courbet-userdebug \
-    nad_courbet-eng
+    $(LOCAL_DIR)/lineage_courbet.mk
